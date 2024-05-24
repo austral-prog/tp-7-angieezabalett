852426734fe70648ef82812cbd936ee9670a249d
Angelina Zabalett <116826508+angieezabalett@users.noreply.github.com>
2024-05-24 17:41:13 -0300

    Update loops_and_print.py

--- loops_and_print.py
+++ loops_and_print.py
@@ -1,6 +1,16 @@
 def enumerate_list(list):
-    return list
+    lista = []
+    for posicion, elemento in enumerate(list):
+        if elemento != "":
+            largo = len(lista)
+            lista.append(f"{largo}. {elemento}")
+    return lista
 
 
 def enumerate_backwards(list):
-    return list
+    lista = []
+    for posicion, elemento in enumerate(list):
+        if elemento != "":
+            largo = len(lista)
+            lista.append(f"{largo}. {elemento[::-1]}")
+    return lista

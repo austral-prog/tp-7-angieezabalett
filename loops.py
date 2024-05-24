b766eb78ca7a71083e46a663c55f7ebe7dc2281f
Angelina Zabalett <116826508+angieezabalett@users.noreply.github.com>
2024-05-24 18:32:21 -0300

    Update loops.py

--- loops.py
+++ loops.py
@@ -1,18 +1,38 @@
 def index_of_by_index(word, list, index):
-    return -1
-
+    for position, element in enumerate(list):
+        if position >= index:
+            if element == word:
+                return position
+    else:
+        return -1
 
 def index_of_empty(list):
+    for position, element in enumerate(list):
+        if element == "":
+            return position
     return -1
 
 
 def index_of(word, list):
-    return -1
+    for indice, element in enumerate(list):
+        if element == word:
+            return indice
+    else:
+        return -1
 
 
 def put(word, list):
+    for position in range(0, len(list)):
+        if list[position] == "":
+            list[position] = word
+            return position
     return -1
 
 
 def remove(word, list):
-    return -1
+    cant = 0
+    for position, element in enumerate(list):
+        if element == word:
+            list[position] = ""
+            cant = cant + 1
+    return cant
